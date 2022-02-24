4178179df6915b47bac1af32a235b69272aedb12
Alexey Panin <panin.lesha2112@yandex.ru>
2022-02-24 20:02:50 +0300

    added screen elements acces task

---
+++ myapplication/src/main/java/ru/mirea/panin/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package ru.mirea.panin.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView myTextView = (TextView) findViewById(R.id.textView);
+        myTextView.setText("New text in MIREA");
+        Button button = findViewById(R.id.btn);
+        button.setText("MireaButton");
+        CheckBox checkBox = findViewById(R.id.check_box);
+        checkBox.setChecked(true);
+    }
+}
