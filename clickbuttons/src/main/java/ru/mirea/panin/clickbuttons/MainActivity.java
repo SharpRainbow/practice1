e7c237e0d5f5312b3cba7a1c7c9de42a6d9dca57
Alexey Panin <panin.lesha2112@yandex.ru>
2022-02-24 20:05:16 +0300

    added event handlers task

---
+++ clickbuttons/src/main/java/ru/mirea/panin/clickbuttons/MainActivity.java
@@ -0,0 +1,40 @@
+package ru.mirea.panin.clickbuttons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tvOut;
+    private Button buttonOk;
+    private Button buttonCancel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        buttonOk = (Button) findViewById(R.id.btnOk);
+        buttonCancel = (Button) findViewById(R.id.btnCancel);
+        View.OnClickListener oclBtnOk = new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                tvOut.setText("Нажата кнопка Ок");
+            }
+        };
+        View.OnClickListener oclBtnCancel = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvOut.setText("Нажата кнопка Cancel");
+            }
+        };
+        buttonOk.setOnClickListener(oclBtnOk);
+        buttonCancel.setOnClickListener(oclBtnCancel);
+    }
+    public void onMyBtnClick(View view){
+        Toast.makeText(this, "Ещё один способ!", Toast.LENGTH_SHORT).show();
+    }
+}
